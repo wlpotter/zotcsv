48071c3d4e4ca651e0b295fd8455b2566a14f295
William L Potter <44096602+wlpotter@users.noreply.github.com>
2025-10-03 12:05:13 -0500

    Rewrite for first successful version
    Now uses asyncio.Sempahore for controlling total concurrent processes, rather than manually setting number of workers.
    
    Also implements tenacity to handle api call retries elegantly

--- zotero-dump-parallel.py
+++ zotero-dump-parallel.py
@@ -2,101 +2,129 @@ import asyncio
 import argparse
 import aiohttp
 import json
+import os
+from tenacity import retry, retry_if_exception, stop_after_attempt, wait_random_exponential
 
-# pass this to the 
+"""
+TODO:
+- re-running but just processing errors
+- handling since filtering
+- handling modified after filtering
+- setting config with command line, config file, etc. using argparse
+"""
+# pass this to the main function
 config = {
     "zotero_api_base": "https://api.zotero.org/groups/4861694/items/top",
     "session_headers": {"Zotero-API-Version": '3'},
     "limit_interval": 100,
     "start_at": 0,
     "max_records": 500,
-    "total_callers": 4,
+    "total_callers": 6,
+    "save_directory": "/home/arren/Documents/GitHub/zotcsv/2025-10-03_dump/",
+    "file_name_base": "zotero_dump2025-10-03_",
+    "log_path": "/home/arren/Documents/GitHub/zotcsv/2025-10-03_dump/log/",
+    "log_file_name": "log.json"
 }
-
-# SAVE_DIRECTORY = "/home/arren/Documents/GitHub/zotcsv/2025-10-02_dump/"
-# FILE_NAME_BASE = "zotero_dump2025-10-02_"
 
 # MAX_RETRIES = 3
 # DEFAULT_BACKOFF_DELAY = 15
 
-# This is the main worker/consumer coroutine that processes the API calls and calls chained
-# async coroutines for handling the request, retries, validation, and saving of the JSON data
-async def api_caller(name: str, api_calls, session, results_log):
-    # will run until cancelled by the main function
-    # ADD TRY/CATCH and use finally to mark the task as done
-    while True:
-        try:
-            # get an API call context from the queue
-            call_context = await api_calls.get()
-            print(f"{name} retrieved an API call to make")
+# This function controls the number of concurrent calls by bounding the main
+# async coro within a Semaphore, limiting max concurrent operations
+async def bounded_get_zotero_data(sem, call_context, session, sleep_time=1):
+    async with sem:
+        return await get_zotero_data(call_context, session, sleep_time)
 
-            # do something with it
-            url, result = await get_zotero_data(call_context=call_context, session=session, caller_name=name)
-            print(f"{name} successfully retrieved data for {url}")
+# This function handles the main logic of getting and processing Zotero data
+# from the API, checking the key sequence, and saving to file
+async def get_zotero_data(call_context, session, sleep_time=1):
+    # Catches and returns exceptions to the log file, specifically processing HTTP and Value errors for key mismatch
+    try:
+        # Attempt to download data from Zotero
+        url, result = await make_api_call(call_context, session)
+        print(f'Successfully retrieved data for {url}')
 
-            returned_keys = [rec["key"] for rec in result]
-            if(returned_key_sequence_matches_expected(returned_keys, call_context["expected_keys"])):
-                print(f"{name}'s data matches expected key sequence; saving data to disk")
-                file_path = await save_json_data(result, call_context)
-                await results_log.put(log_success(file_path, url, call_context, name))
-            else:
-                raise ValueError("Returned item keys did not match the expected sequence")
-            """
-            If the result is successful (i.e., a list of JSON docs), then validate the keys
-                if valid, save them to a file
-                otherwise raise an error
-            else if the subroutine is unsuccessful (i.e., retries exceeded and got an error), log that error
-            """
-        # Explicit handling of ValueError exception -- raised by 
-        except ValueError as ve:
-            print(f"{name} encountered a key mismatch error")
-            logged_error = {
-                "result": "Error",
-                "context": call_context,
-                "url": url,
-                "returned_keys": returned_keys,
-                "caller": name,
-                "error": str(ve)
-            }
-            await results_log.put(logged_error)
-        except Exception as e:
-            print("An error occurred for {name}")
-            logged_error = {
-                "result": "Error",
-                "context": call_context,
-                "caller": name,
-                "error": str(e)
-            }
-            await results_log.put(logged_error)
-        finally:
-            # give a bit of sleep delay for breathing room
-            await asyncio.sleep(1)
-            # Mark this task done
-            api_calls.task_done()
+        returned_keys = [rec["key"] for rec in result]
+        if(returned_key_sequence_matches_expected(returned_keys, call_context["expected_keys"])):
+            print(f"Records returned for {url} matches expected key sequence; saving data to disk")
+            file_path = await save_json_data(result, call_context)
+            return log_success(file_path=file_path, call_context=call_context, url=url)
+        else:
+            raise ValueError("Returned item keys did not match the expected sequence")
+    except aiohttp.ClientResponseError as re:
+        print(f"HTTP {re.status} error fetching data from {re.request_info.url}. Retry attempts exceeded, logging final response headers and message.")
+        logged_error = {
+            "result": "Error",
+            "context": call_context,
+            "url": str(re.request_info.url),
+            "status_code": re.status,
+            "response_headers": re.headers,
+            "error": str(ve)
+        }
+        return logged_error
+    except ValueError as ve:
+        print(f"Keys returned by {url} did not match the expected sequence, see log for details")
+        logged_error = {
+            "result": "Error",
+            "context": call_context,
+            "url": url,
+            "returned_keys": returned_keys,
+            "error": str(ve)
+        }
+        return logged_error
+    # Handle any other exception by returning it to the log
+    except Exception as e:
+        print(f"An unspecified error occurred processing an API call, see log for details")
+        logged_error = {
+            "result": "Error",
+            "context": call_context,
+            "error": str(e)
+        }
+        return logged_error
+    finally:
+        # give a bit of breathing room
+        await asyncio.sleep(sleep_time)
 
-async def get_zotero_data(call_context, session, caller_name):
+# This function is used by the retry logic to determine whether to retry the request
+# Retries if a response exception was raised, and the status code is in the set of server errors, or the 429 Too Many Requests error
+# This will allow other errors, such as 404 or 403, to pass immediately back to the caller
+def is_retriable_error(exception):
+    return isinstance(exception, aiohttp.ClientResponseError) and exception.status in {429, 500, 502, 503, 504}
+
+# The logic for making the API call itself, which is retriable using tenacity
+# Will be retried if a response error occurs and the status code is 429 or a server error
+# Tries at least 3 times at a randomly expanding window
+# If attempts exceeded, will re-raise the error to be handled by the caller coro
+@retry(retry=retry_if_exception(is_retriable_error),
+    stop=stop_after_attempt(3),
+    wait=wait_random_exponential(multiplier=1, max=60),
+    reraise=True)
+async def make_api_call(call_context, session):
     async with session.get(call_context["url_base"], params=call_context["request_params"]) as response:
-        print(f"{caller_name} making API call to {response.url}")
-        response.raise_for_status() # raise any exceptions for unsuccessful connects
-        # TODO: turn into try/catch block that handles retries of 500 erros
+        print(f"Making API call to {response.url}")
+        # raise any exceptions for unsuccessful connects, which triggers tenacity to retry
+        response.raise_for_status()
+        # if no error occured, return the URL and the JSON data
         data = await response.json()
-        return (response.url, data)
+        return str(response.url), data
 
 def returned_key_sequence_matches_expected(returned, expected):
     return returned == expected
 
+# Saves the JSON data to disk, returning the filepath of the JSON file for reference
 async def save_json_data(result, call_context):
-    # TODO: implement this function to save based on data in the call context
-    return "Saved successfully; TBD to actually do this"
+    filepath = call_context["save_directory"] + call_context["file_name"]
+    with open(filepath, "w+") as f:
+        json.dump(result, f, indent=2)
+    return filepath
 
 
-def log_success(file_path, call_context, caller_name, url):
+def log_success(file_path, call_context, url):
     logged_success = {
                 "result": "Success",
                 "context": call_context,
                 "url": url,
-                "caller": caller_name,
-                "message": f"Data successfully to {file_path}"
+                "message": f"Data saved successfully to {file_path}"
             }
     return logged_success
 
@@ -126,59 +154,60 @@ async def main(config):
             max_limit = min(max_limit, config["max_records"])
 
         # Create an async work queue for the API calls based on the start, max limit, and interval parameters
-        print("Preparing queue of needed API calls")
-        api_calls_queue = asyncio.Queue()
+        print(f"Preparing queue of needed API calls (Items {config['start_at']} to {max_limit} at an interval of {config['limit_interval']})")
+        api_calls = []
         for i in range(config["start_at"], max_limit, config["limit_interval"]):
             api_call = {}
 
+            # set the start and end window
+            window_start = i
+            window_end = window_start + config["limit_interval"]
+
             # get a copy of the request parameters and set the "start" parameter based on the loop location
             req_params = init_req_params.copy()
-            req_params["start"] = i
+            req_params["start"] = window_start
 
             # get a subset of the returned item keys to validate this API chunk against
-            window_start = i
-            window_end = i + config["limit_interval"]
             windowed_keys = expected_keys[window_start:window_end]
             
             # add these to a dictionary, which is pushed the work queue for later processing by the async workers
             api_call["url_base"] = config["zotero_api_base"]
             api_call["request_params"] = req_params
             api_call["expected_keys"] = windowed_keys
-            api_calls_queue.put_nowait(api_call)
+            api_call["save_directory"] = config["save_directory"]
+            api_call["file_name"] = f'{config["file_name_base"]}{window_start}-{window_end - 1}.json'
+            api_calls.append(api_call)
 
         # save the initial number of calls
-        total_api_calls = api_calls_queue.qsize()
+        total_api_calls = len(api_calls)
+        print(f"A total of {total_api_calls} API calls will be made.")
 
-        # initialize a results Queue for logging the result of each task
-        results_log = asyncio.Queue()
 
-        # Create the api caller 'workers' 
-        print(f"Initializing the {config['total_callers']} concurrent API calling functions")
-        api_callers = []
-        for i in range(config["total_callers"]):
-            caller = asyncio.create_task(api_caller(f'caller-{i+1}', api_calls_queue, session, results_log))
-            api_callers.append(caller)
+        # Create a Semaphore to control number of simultaneous callers allowed
+        sem = asyncio.Semaphore(config["total_callers"])
+
+        print("Beginning API calls")
+        callers = [
+            bounded_get_zotero_data(sem, call, session)
+            for call in api_calls
+        ]
         
-        print("Beginning to make and process API Calls")
-        await api_calls_queue.join()
+        results = await asyncio.gather(*callers)
 
-        # Cleanup by cancelling callers
-        for caller in api_callers:
-            caller.cancel()
-        
-        await asyncio.gather(*api_callers, return_exceptions=True)
+        print("Gathering and saving results log to file")
+        log_file_path = config["log_path"] + config["log_file_name"]
+        with open(log_file_path, 'w+') as log:
+            json.dump(results, log, indent=2)
+        print(f"Log file saved to {log_file_path}")
 
-        """
-        TODO:
-        - get the items from results_log queue and conver to a list to dump as a JSON file
-        """
-
+        num_success = len([r for r in results if r["result"] == "Success"])
+        num_error = len([r for r in results if r["result"] == "Error"])
         print('=========')
-        print(f'{len(api_callers)} API Callers performed all {total_api_calls} API Calls. See the logs for full results')
-        """
-        - define the async chain of work for:
-            - handling retries
-            - saving the file
-        - add to this the functionalities for 'since' and last modified saving
-        """
+        print(f'All {total_api_calls} API Calls have been made, with {num_success} successful; {num_error} error(s). See the log file for full results')
+
+# Make sure the directories are created for saving files to them
+os.makedirs(config["save_directory"], exist_ok=True)
+os.makedirs(config["log_path"], exist_ok=True)
+
+# run the main function, passing in the config dictionary
 asyncio.run(main(config))
